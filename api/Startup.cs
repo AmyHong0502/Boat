84cca01cb71531b6fd1205f14355936677f8e3a1
Tony Pacheco <tonypacheco.cillis@gmail.com>
2019-02-23 15:21:16 -0800

    move to static db

--- api/Startup.cs
+++ api/Startup.cs
@@ -31,7 +31,7 @@ namespace api
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<ApplicationDbContext>(
                 option => option.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
@@ -80,6 +80,7 @@ namespace api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStaticFiles();
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
